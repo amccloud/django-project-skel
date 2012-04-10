23edf680436cb1ad0929be89e9684eade13a6719
Nikita Hismatov <hismatovns@gmail.com>
2012-04-10 18:36:00 +0600

    sqlite3 - local path >:]

--- project_name/local_settings.py
+++ project_name/local_settings.py
@@ -5,7 +5,7 @@ DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(PROJECT_DIR, '{{ project_name }}.db'), # Or path to database file if using sqlite3.
+        'NAME': '{{ project_name }}.db',        # Or path to database file if using sqlite3.
         'USER': '',                             # Not used with sqlite3.
         'PASSWORD': '',                         # Not used with sqlite3.
         'HOST': '',                             # Set to empty string for localhost. Not used with sqlite3.
