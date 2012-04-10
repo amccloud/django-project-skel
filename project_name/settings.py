fb1df94e468d7846c4b30dc65dfc0817342edcf1
Nikita Hismatov <hismatovns@gmail.com>
2012-04-11 02:43:31 +0600

    secret key in settings

--- project_name/settings.py
+++ project_name/settings.py
@@ -13,6 +13,8 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+SECRET_KEY = '{{ secret_key }}'
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
