4da11f019ac0275884fc97b501059c5851d41eec
Nikita Hismatov <hismatovns@gmail.com>
2012-04-11 03:09:08 +0600

    admin tools urls

--- project_name/urls.py
+++ project_name/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^admin_tools/', include('admin_tools.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
