c2e793cdcc4a823f9876a8b3577690a159dcf40c
Nikita Hismatov <hismatovns@gmail.com>
2012-04-10 19:32:02 +0600

    ru-Ru + timezone

--- project_name/settings.py
+++ project_name/settings.py
@@ -20,11 +20,11 @@ MANAGERS = ADMINS
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Yekaterinburg'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-Ru'
 
 SITE_ID = 1
 
